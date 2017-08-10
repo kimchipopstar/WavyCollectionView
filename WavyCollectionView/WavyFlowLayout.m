cce6eee8ae2857736f8def4cda28651759035674
J.K <K_940201@hotmail.com>
2017-08-10 13:59:02 -0700

    complete but some times it goes off the screen

--- WavyCollectionView/WavyFlowLayout.m
+++ WavyCollectionView/WavyFlowLayout.m
@@ -16,4 +16,28 @@
     self.minimumInteritemSpacing = CGFLOAT_MAX;
 }
 
+-(NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect{
+    
+    NSArray<UICollectionViewLayoutAttributes *> *superAttrs = [super layoutAttributesForElementsInRect:rect];
+    
+    NSMutableArray<UICollectionViewLayoutAttributes *> *newAttrs = [[NSMutableArray alloc]init];
+    
+    for (UICollectionViewLayoutAttributes *superAttribute in superAttrs) {
+    
+        UICollectionViewLayoutAttributes *attribute = superAttribute.copy;
+        [newAttrs addObject:attribute];
+        
+        attribute.center = CGPointMake(attribute.center.x, arc4random_uniform(self.collectionView.frame.size.height));
+        
+//        attribute.frame = CGRectMake(attribute.frame.origin.x, arc4random_uniform(self.collectionView.bounds.size.height),attribute.frame.size.width ,attribute.frame.size.height);
+
+    
+    }
+    
+    return newAttrs;
+    
+}
+
+
+
 @end
